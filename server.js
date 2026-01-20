6d98d8f56967c03ee78114de7034a5660dbe4e63
ROIEngineer <191797684+ROIEngineer@users.noreply.github.com>
2026-01-20 01:16:45 -0500

    Add back PORT fallback

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
